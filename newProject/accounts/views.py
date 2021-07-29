6556c9dcfc111be905cce6f98c16447a1680f1e1
Muhammed Shahmil <mhdshahmil@gmail.com>
2021-07-29 16:25:46 +0530

    updated

--- newProject/accounts/views.py
+++ newProject/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from  travello.models import Destination
 
 # Create your views here.
+def register(request):
+    
+
+  
+    dests= Destination.objects.all()
+
+    return render(request,'register.html',{'dests':dests})
